8955daa0fce2efcf02cbc410590deff32285ac26
Khuê <mkhue720>
2023-01-22 20:01:40 +0700

    click here

---
+++ thongtinthanhtoan/ntd.php
@@ -0,0 +1,23 @@
+<?
+$ntdavt = 'https://i.imgur.com/KbCsYjr.jpg'; // avatar của hình
+/* ------------------------------------- */
+$ntdbanner = 'https://i.imgur.com/I0i4Q3k.jpg'; // banner của hình
+/* ------------------------------------- */
+$ntdicon = 'https://i.imgur.com/KbCsYjr.jpg'; // thay shortcuticon và icon
+/* ------------------------------------- */
+$ten = 'NGÔ MINH KHUÊ'; // ghi full tên bằng in hoa
+/* ------------------------------------- */
+$tieusu = 'Minh Khuê'; // tên tiểu sử
+/* ------------------------------------- */
+$tt = 'Ngày Sinh : 28/04/2004 <br>Quê Quán : Bắc Ninh <br>SĐT : 0984703325 <br>Trạng Thái : Hẹn hò <br>Hiện tại mình vừa học vừa làm cũng như phát triển bản thân để ngày một hoàn thiện hơn . Đối với mình mỗi ngày chỉ cần cố gắng hơn 1 chút thì tương lai chắc chắn sẽ tốt hơn'; //muốn xuống dòng thì bên <br>
+/* ------------------------------------- */
+$tkmbbank ='0984703325'; //nhập tk mbbank
+$tenmbbank ='NGÔ MINH KHUÊ'; //tên mbbank
+/* ------------------------------------- */
+$tkmomo ='0984703325'; //nhập tk momo
+$tenmomo = 'NGÔ MINH KHUÊ'; // tên momo
+/* ------------------------------------- */
+$idfb ='1485219421'; // nhập id fb vào
+/* ------------------------------------- */
+$tencopyright ='Minh Khuê'; //tên ở dòng coppyright
+?>
