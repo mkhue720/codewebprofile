42143d42d9ce0489097c521bab5d0dec5be39cfe
NMK <mkhue720@gmail.com>
2023-10-17 23:18:46 +0700

    update

--- thongtinthanhtoan/script.js
+++ thongtinthanhtoan/script.js
@@ -1,17 +1,13 @@
 function copyText(stk) {
-  // Tạo một textarea ẩn để sao chép STK
   var textArea = document.createElement("textarea");
   textArea.value = stk;
   document.body.appendChild(textArea);
 
-  // Chọn và sao chép nội dung trong textarea
   textArea.select();
   document.execCommand("copy");
 
-  // Loại bỏ textarea sau khi sao chép
   document.body.removeChild(textArea);
 
-  // Thông báo cho người dùng đã sao chép thành công
   alert("Đã sao chép STK: " + stk);
 }
 
@@ -21,17 +17,17 @@ var button3 = document.getElementById("myButton3");
 var button4 = document.getElementById("myButton4");
 
 button1.addEventListener("click", function() {
-  copyText("0984703325"); // Sao chép STK cho MBBANK
+  copyText("0984703325"); 
 });
 
 button2.addEventListener("click", function() {
-  copyText("1031955640"); // Sao chép STK cho VIETCOMBANK
+  copyText("1031955640"); 
 });
 
 button3.addEventListener("click", function() {
-  copyText("0984703325"); // Sao chép STK cho MOMO
+  copyText("0984703325"); 
 });
 
 button4.addEventListener("click", function() {
-  copyText("9704229245518167"); // Sao chép STK cho VIETTELPAY
+  copyText("9704229245518167"); 
 });
